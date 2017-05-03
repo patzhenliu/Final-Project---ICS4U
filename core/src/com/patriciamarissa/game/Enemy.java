644c0f1354b3e6d543f47a235b460deafc20334e
Marissa J <junmarissa0@gmail.com>
2017-05-03 01:19:54 -0400

    finally got all the stupid enemy sprites

--- core/src/com/patriciamarissa/game/Enemy.java
+++ core/src/com/patriciamarissa/game/Enemy.java
@@ -23,21 +23,34 @@ public class Enemy {
 		this.x = x ;
 		this.y = y ;
 		if (type == 0) { // ENEMY TYPE 1: ONLY WALKS AROUND ON A PLATFORM
-			// INCOMPLETE
 			spritesheet = new Texture  (Gdx.files.internal("walking.png")) ;
+			sprites = new Sprite [4] ;
+			deathSprs = new Sprite [5] ;
+			
+			sprites [0] = new Sprite (spritesheet, 0, 80, 67, 77) ;
+			sprites [1] = new Sprite (spritesheet, 70, 77, 70, 80) ;
+			sprites [2] = new Sprite (spritesheet, 144, 81, 71, 77) ;
+			sprites [3] = new Sprite (spritesheet, 220, 82, 78, 77) ;
+			
+			deathSprs [0] = new Sprite (spritesheet, 0, 162, 64, 96) ;
+			deathSprs [1] = new Sprite (spritesheet, 0, 263, 66, 97) ;
+			deathSprs [2] = new Sprite (spritesheet, 77, 265, 62, 95) ;
+			deathSprs [3] = new Sprite (spritesheet, 150, 272, 66, 87) ;
+			deathSprs [4] = new Sprite (spritesheet, 225, 289, 64, 69) ;
+			
 			hp = 1 ;
 			leftright = false ;
+			currentsprite = sprites [0] ;
 		}
 		else if (type == 1) { // ENEMY TYPE 2: FLIES UP AND DOWN BETWEEN PLATS
-			// COMPLETE
 			spritesheet = new Texture  (Gdx.files.internal("flying.png")) ;
 			sprites = new Sprite [4] ;
 			deathSprs = new Sprite [5] ;
 			
 			sprites [0] = new Sprite (spritesheet, 5, 10, 79, 114) ;
-			sprites [0] = new Sprite (spritesheet, 102, 46, 81, 78) ;
-			sprites [0] = new Sprite (spritesheet, 205, 45, 90, 80) ;
-			sprites [0] = new Sprite (spritesheet, 311, 31, 103, 92) ;
+			sprites [1] = new Sprite (spritesheet, 102, 46, 81, 78) ;
+			sprites [2] = new Sprite (spritesheet, 205, 45, 90, 80) ;
+			sprites [3] = new Sprite (spritesheet, 311, 31, 103, 92) ;
 			
 			deathSprs [0] = new Sprite (spritesheet, 8, 409, 91, 100) ;
 			deathSprs [1] = new Sprite (spritesheet, 116, 372, 101, 136) ;
@@ -47,15 +60,35 @@ public class Enemy {
 			
 			hp = 1 ;
 			updown = false ;
+			currentsprite = sprites [0] ;
 		}
-		else if (type == 2) { // ENEMY TYPE 3: ONLY SHOOTS STRAIGHT BEAM
-			// INCOMPLETE
+		else if (type == 2) { // ENEMY TYPE 3: ONLY SHOOTS STRAIGHT BEAM. STANDS STILL.
 			spritesheet = new Texture  (Gdx.files.internal("fire laser.png")) ;
+			sprites = new Sprite [9] ;
+			deathSprs = new Sprite [7] ;
+
+			sprites [0] = new Sprite (spritesheet, 0, 552, 181, 152) ;
+			sprites [1] = new Sprite (spritesheet, 187, 552, 181, 152) ;
+			sprites [2] = new Sprite (spritesheet, 379, 552, 181, 152) ;
+			sprites [3] = new Sprite (spritesheet, 567, 552, 181, 152) ;
+			sprites [4] = new Sprite (spritesheet, 757, 552, 181, 152) ;
+			sprites [5] = new Sprite (spritesheet, 947, 552, 181, 152) ;
+			sprites [6] = new Sprite (spritesheet, 1133, 523, 184, 181) ;
+			sprites [7] = new Sprite (spritesheet, 1323, 552, 181, 152) ;
+			sprites [8] = new Sprite (spritesheet, 1518, 552, 181, 152) ;
+
+			deathSprs [0] = new Sprite (spritesheet, 0, 727, 204, 158) ;
+			deathSprs [1] = new Sprite (spritesheet, 212, 727, 237, 158) ;
+			deathSprs [2] = new Sprite (spritesheet, 416, 727, 228, 158) ;
+			deathSprs [3] = new Sprite (spritesheet, 712, 727, 224, 158) ;
+			deathSprs [4] = new Sprite (spritesheet, 956, 727, 222, 158) ;
+			deathSprs [5] = new Sprite (spritesheet, 1197, 727, 222, 158) ;
+			deathSprs [6] = new Sprite (spritesheet, 1437, 727, 222, 158) ;
+			
 			hp = 3 ;
-			leftright = false ;
+			currentsprite = sprites [0] ;
 		}
 		else if (type == 3) { // ENEMY TYPE 4: CHARGES TOWARDS PLAYER. JUST RUNS ON FLOOR-LEVEL.
-			// COMPLETE
 			spritesheet = new Texture  (Gdx.files.internal("charging.png")) ;
 			sprites = new Sprite [6] ;
 			deathSprs = new Sprite [10] ;
@@ -75,15 +108,14 @@ public class Enemy {
 			deathSprs [5] = new Sprite (spritesheet, 882, 1004, 165, 124) ;
 			deathSprs [6] = new Sprite (spritesheet, 30, 1169, 165, 124) ;
 			deathSprs [7] = new Sprite (spritesheet, 179, 1169, 159, 124) ;
-			deathSprs [6] = new Sprite (spritesheet, 320, 1169, 165, 124) ;
-			deathSprs [7] = new Sprite (spritesheet, 440, 1134, 155, 147) ;
+			deathSprs [8] = new Sprite (spritesheet, 320, 1169, 165, 124) ;
+			deathSprs [9] = new Sprite (spritesheet, 440, 1134, 155, 147) ;
 			
 			hp = 2 ;
 			leftright = false ;
 			currentsprite = sprites [0] ;
 		}
 		else if (type == 4) { // ENEMY TYPE 5: JUST STANDS THERE. HAS TO BE SHOT A LOT. BETTER TO AVOID.
-			// COMPLETE
 			spritesheet = new Texture  (Gdx.files.internal("stand still.png")) ;
 			deathSprs = new Sprite [6] ;
 			
@@ -184,5 +216,9 @@ public class Enemy {
 	
 	public void setSpeed (int s) {
 		speed = s ;
+	}
+	
+	public int getType () {
+		return type ;
 	}
 }
