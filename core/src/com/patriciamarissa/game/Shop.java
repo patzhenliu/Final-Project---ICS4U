56f1719615b69fb09c77a04544c11bf60ca3fc92
Marissa J <junmarissa0@gmail.com>
2017-05-29 15:20:40 -0400

    whats going to be in the shop

--- core/src/com/patriciamarissa/game/Shop.java
+++ core/src/com/patriciamarissa/game/Shop.java
@@ -8,7 +8,26 @@ public class Shop {
 	private Batch batch;
 	private Texture shopImg;
 	private final int title, game, shop ;
+	private boolean las1, las2, las3 ;
+	private boolean life1, life2, life3 ;
+	private boolean jump1, jump2 ;
+	private boolean mon1, mon2, mon3 ;
+	private boolean dubmon, dubscore, halftime, nuke, killfire, killholes ;
 	private int coins ;
+	private int [] pricelist ;
+	
+	/* lasers (upgradable to 3) (10, 20, 40)
+	 * more life (upgradable to 6, but starts at 3) (10, 20, 40)
+	 * higher jump (upgradable twice, or to whatever point hits top of screen) (10, 20, 40)
+	 * increase money (upgradable) (20, 40, 60)
+	 * double money (one time use) (10)
+	 * double score (one time use) (10)
+	 * slow down time (one time use) (10)
+	 * kill all enemies? (one time use, timed, 10 seconds) (20)
+	 * remove fire (one time use) (10)
+	 * remove holes (one time use) (10)
+	 * GOING TO HAVE TO CHANGE LION TO ACCOMODATE THE HOLE REMOVAL. STILL NEED TO FIX LION AND GARGOYLE MOVEMENT ANYWAYS.
+	 */
 	
 	public Shop(Batch batch) {
 		this.batch = batch;
@@ -17,6 +36,29 @@ public class Shop {
 		game = 2 ;
 		shop = 3 ;
 		coins = 0 ;
+		
+		las1 = false ;
+		las2 = false ;
+		las3 = false ;
+		life1 = false ;
+		life2 = false ;
+		life3 = false ;
+		jump1 = false ;
+		jump2 = false ;
+		mon1 = false ;
+		mon2 = false ;
+		mon3 = false ;
+		dubmon = false ;
+		dubscore = false ;
+		halftime = false ;
+		nuke = false ;
+		killfire = false ;
+		killholes = false ;
+		
+		pricelist = new int [10] ;
+		Arrays.fill (pricelist, 10) ;
+		pricelist [3] = 20 ; // increase money
+		pricelist [7] = 20 ; // nuke the enemies
 	}
 	
 	public void add (int c) {
